c958dfa4b3cb18392734e439956c928c4b6596c5
imapsingh007 <150086761+imapsingh007@users.noreply.github.com>
2025-09-18 16:42:47 +0530

    Update __init__.py

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -2,6 +2,6 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
-        "<h1>Hello from Python Function App on Azure!</h1>",
+        "<h1>Hello from Python Function App on Azure function!</h1>",
         mimetype="text/html"
     )
